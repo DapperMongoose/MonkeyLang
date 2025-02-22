ecb734fc3a6a95cbf4ca91e9884995ea7ad9d0d8
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2025-02-22 11:31:25 -0700

    consume evaluator in REPL

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"github.com/DapperMongoose/monkey/evaluator"
 	"github.com/DapperMongoose/monkey/lexer"
 	"github.com/DapperMongoose/monkey/parser"
 	"io"
@@ -43,8 +44,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
