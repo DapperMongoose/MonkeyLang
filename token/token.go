7e5207f064c4ffb40a361fb50b3efd76e885c2fc
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2025-02-23 11:19:32 -0700

    add string token and teach lexer to extract strings

--- token/token.go
+++ token/token.go
@@ -29,8 +29,9 @@ const (
 	EOF     = "EOF"
 
 	// Identifiers + literals
-	IDENT = "IDENT" // add, foobar, x, y, ...
-	INT   = "INT"   // 123456
+	IDENT  = "IDENT" // add, foobar, x, y, ...
+	INT    = "INT"   // 123456
+	STRING = "STRING"
 
 	// Operators
 	ASSIGN   = "="
